0b6511a786dea7c52d9523907de9cb803ed482a7
Matheus de Assis <assis4002@gmail.com>
2022-06-23 23:15:45 -0300

    ajustes de scroll nav

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -2,10 +2,13 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-    document.getElementById("navbar").style.padding = "30px 10px";
+    //document.getElementById("navbar").style.padding = "30px 10px";
+    document.getElementById("perfil").style.width = "0";
     // document.getElementById("logo").style.fontSize = "25px";
-  } else {
-    document.getElementById("navbar").style.padding = "80px 10px";
+  }
+  else {
+    //document.getElementById("navbar").style.padding = "80px 10px";
+    document.getElementById("perfil").style.width = "3rem";
     //document.getElementById("logo").style.fontSize = "35px";
   }
 }
