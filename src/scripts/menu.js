1091c23c35cf4b4750ccea03879afda76bff183e
Matheus de Assis <assis4002@gmail.com>
2022-06-24 00:05:49 -0300

    ajustes finais nav

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,14 +1,16 @@
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-    //document.getElementById("navbar").style.padding = "30px 10px";
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("navbar").style.padding = "8px";
+    document.getElementById("navbar").style.boxShadow = "0 1px 6px 0 rgb(0 0 0 / 15%)"
     document.getElementById("perfil").style.width = "0";
-    // document.getElementById("logo").style.fontSize = "25px";
+    document.getElementById("menu-text").style.fontSize = "small";
   }
   else {
-    //document.getElementById("navbar").style.padding = "80px 10px";
+    document.getElementById("navbar").style.padding = "24px";
+    document.getElementById("navbar").style.boxShadow = "none"
     document.getElementById("perfil").style.width = "3rem";
-    //document.getElementById("logo").style.fontSize = "35px";
+    document.getElementById("menu-text").style.fontSize = "medium";
   }
 }
